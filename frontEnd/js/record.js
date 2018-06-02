1e8b17354ab1991d339f9f5741f12813c139bd3e
rainble <sunryhfut@126.com>
2018-06-02 21:38:45 +0800

    HeartVoice v0.2

--- frontEnd/js/record.js
+++ frontEnd/js/record.js
@@ -147,7 +147,7 @@
         };  
   
         //上传  
-        this.upload = function () {  
+        this.upload_once = function () {
             var fd = new FormData();  
             fd.append('audioData', this.getBlob());  
             // var xhr = new XMLHttpRequest();  
@@ -191,7 +191,46 @@
                     }).done(function(res) {
 
                     });
-        };  
+        };
+
+        this.upload = function () {
+            var fd = new FormData();
+            fd.append('audioData', this.getBlob());
+            $.ajax({
+                url:"http://10.131.252.141/home/inspur/Documents/hackx_upload/",
+                type: "post",
+                cache:false,
+                processData: false,
+                contentType: false,
+                headers:{"Authorization":"Basic aW5zcHVyOmluc3B1cg=="},
+                data:fd,
+                error: function (xhr, status, errorThrow) {
+                    console.log("Error:", errorThrow);
+                    console.log("Status", status);
+                    console.log(xhr);
+                },
+                success: function() {
+                    if (json[0][code] == 1)
+                    {
+                        document.getElementById("ai_feedback").setAttribute("src", json[1][addr]);
+                        var audio = document.querySelector('audio');
+                        audio.play();
+                    }
+                    else if (json[0][code] == 2)
+                    {
+                        // for(int i = 0; i < 10; i+=2){
+                        //     var[] name = json[i];
+                        //     var[] path = json[i+1];
+                        // }
+                    }
+                    else {
+                        console.log("get message error.");
+                    }
+                }
+            }).done(function(res) {
+
+            });
+        }
   
         // this.askforvoice = function () {
         //     $.ajax({
